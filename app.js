f0e48fec3c47d5215666ae4f0c928a0a82b9c67f
puneet-tanwar <130287831+puneet-tanwar@users.noreply.github.com>
2023-04-14 14:47:53 +0530

    updated app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 app.use("/users", userRoutes);
-app.listen(5001, (req, res) => {
-  console.log("Server listening on : 5001");
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, (req, res) => {
+  console.log("Server listening on : ",PORT);
 });
